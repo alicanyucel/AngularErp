416a52dbbadead6ad7c53a16192e329e11c2339b
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-01-29 00:44:56 +0300

    Tema Giydirildi.

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=====================================
+!!! THANK YOU FOR PURCHASING FUSE !!!
+=====================================
+
+FILES:
+fuse-demo-v17.2.0.zip   : Fuse Demo Project
+fuse-starter-v17.2.0.zip: Fuse Starter Project
+
+ONLINE DOCUMENTATION:
+http://angular-material.fusetheme.com/docs/guides/getting-started/introduction
+
+SUPPORT:
+For your support requests please visit http://withinpixels.ticksy.com
+Your requests will be queued and can take up to 48 hours in working days before answered.
+powered by to Ali can 
---
+++ src/app/modules/admin/ui/fuse-components/components/loading-bar/loading-bar.component.ts
@@ -0,0 +1,104 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { finalize } from 'rxjs';
+import { FuseLoadingService } from '@fuse/services/loading';
+import { FuseComponentsComponent } from 'app/modules/admin/ui/fuse-components/fuse-components.component';
+
+@Component({
+    selector   : 'loading-bar',
+    templateUrl: './loading-bar.component.html'
+})
+export class LoadingBarComponent
+{
+    apiCallStatus: string = '-';
+    mode: 'determinate' | 'indeterminate' = 'indeterminate';
+    sliderValue: number = 0;
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private _httpClient: HttpClient,
+        private _fuseComponentsComponent: FuseComponentsComponent,
+        private _fuseLoadingService: FuseLoadingService
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Toggle the drawer
+     */
+    toggleDrawer(): void
+    {
+        // Toggle the drawer
+        this._fuseComponentsComponent.matDrawer.toggle();
+    }
+
+    /**
+     * Show the loading bar
+     */
+    showLoadingBar(): void
+    {
+        this._fuseLoadingService.show();
+    }
+
+    /**
+     * Hide the loading bar
+     */
+    hideLoadingBar(): void
+    {
+        this._fuseLoadingService.hide();
+    }
+
+    /**
+     * Set the auto mode
+     *
+     * @param change
+     */
+    setAutoMode(change: MatSlideToggleChange): void
+    {
+        this._fuseLoadingService.setAutoMode(change.checked);
+    }
+
+    /**
+     * Make a fake API call
+     */
+    makeAPICall(): void
+    {
+        this.apiCallStatus = 'Waiting...';
+
+        this._httpClient.get('https://jsonplaceholder.typicode.com/posts?_delay=2000')
+            .pipe(finalize(() => {
+                this.apiCallStatus = 'Finished!';
+            }))
+            .subscribe((response) => {
+                console.log(response);
+            });
+    }
+
+    /**
+     * Toggle the mode
+     */
+    toggleMode(): void
+    {
+        // Show the loading bar
+        this._fuseLoadingService.show();
+
+        // Set the mode
+        this.mode = this.mode === 'indeterminate' ? 'determinate' : 'indeterminate';
+        this._fuseLoadingService.setMode(this.mode);
+    }
+
+    /**
+     * Set the progress
+     */
+    setProgress(): void
+    {
+        this._fuseLoadingService.setProgress(this.sliderValue);
+    }
+}
